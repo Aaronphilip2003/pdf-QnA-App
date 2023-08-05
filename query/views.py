e4df7af15a13bc0b40244665c77ce4e313478a92
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-08-05 13:48:20 +0530

    Sends API calls

--- query/views.py
+++ query/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from django.http import JsonResponse
+
 
 
 def say_hello(request):
@@ -11,13 +13,21 @@ def home(req):
     return render(req,'home.html')
 
 
+def remove_special_characters(text_list):
+    special_chars = ["[", "]", "\\", "\n"]
+    for char in special_chars:
+        text_list = [text.replace(char, "") for text in text_list]
+    return text_list
+
 def norwegian_wood(request):
     embedding2 = HuggingFaceEmbeddings()
     vdb_chunks_HF = FAISS.load_local("query/vdb_chunks_HF", embedding2, index_name="indexHF")
     query = request.GET.get('query', '')
     print(query)
     ans = vdb_chunks_HF.as_retriever().get_relevant_documents(query)
-    result = ans[0].page_content if ans else None
-    
-    # Pass the 'result' variable to the template for display
-    return render(request, "norwood.html", {'result': result})
+
+    # Assuming ans is a list of documents and you want to return the first document's page content
+    answer = ans[0].page_content if ans else "No answer found."
+
+    # Return the answer directly as a JSON response
+    return JsonResponse({'answer': answer})
