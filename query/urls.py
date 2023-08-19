eedaad3248e89ea6ed527e3dd2fc46e7d9fded49
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-08-19 10:08:20 +0530

    update 19/08/23

--- query/urls.py
+++ query/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('hello/',views.say_hello),
-    path('',views.home),
     path('norwegianwood/',views.norwegian_wood),
-    path('norwegian_wood', views.norwegian_wood, name='norwegian_wood'),
+    path('turingpaper/', views.turing_paper, name='turing_paper'),
+    path('psych/', views.psych, name='psych')
 
 ]
--- query/views.py
+++ query/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+import requests
 from django.http import JsonResponse
+from langchain.embeddings import GooglePalmEmbeddings
+from transformers import pipeline
+
+
+SUMMARIZER_API_URL = "https://api-inference.huggingface.co/models/philschmid/bart-large-cnn-samsum"
+headers = {"Authorization": "Bearer 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"}
+
+def summarize_text(payload):
+    response = requests.post(SUMMARIZER_API_URL, headers=headers, json=payload)
+    return response.json()
 
 
 
@@ -19,6 +30,8 @@ def remove_special_characters(text_list):
         text_list = [text.replace(char, "") for text in text_list]
     return text_list
 
+from django.http import JsonResponse
+
 def norwegian_wood(request):
     embedding2 = HuggingFaceEmbeddings()
     vdb_chunks_HF = FAISS.load_local("query/vdb_chunks_HF", embedding2, index_name="indexHF")
@@ -26,8 +39,59 @@ def norwegian_wood(request):
     print(query)
     ans = vdb_chunks_HF.as_retriever().get_relevant_documents(query)
 
-    # Assuming ans is a list of documents and you want to return the first document's page content
-    answer = ans[0].page_content if ans else "No answer found."
+    # Assuming ans is a list of documents and you want to return all document's page content
+    final_ans = [doc.page_content for doc in ans] if ans else []
+    print(final_ans)
 
-    # Return the answer directly as a JSON response
-    return JsonResponse({'answer': answer})
+    # Prepare the JSON response with all answers in a single "answers" field
+    response_data = {
+        'answers': final_ans
+    }
+
+    return JsonResponse(response_data)
+
+
+def turing_paper(request):
+    embedding2 = HuggingFaceEmbeddings()
+    vdb_chunks_HF = FAISS.load_local("query/vdb_chunks_HF", embedding2, index_name="indexTuring")
+    query = request.GET.get('query', '')
+    print(query)
+    ans = vdb_chunks_HF.as_retriever().get_relevant_documents(query)
+
+    # Assuming ans is a list of documents and you want to return all document's page content
+    answers = [doc.page_content for doc in ans] if ans else []
+    print(answers)
+
+    # Return all answers as a JSON response
+    return JsonResponse({'answers': answers})
+
+from transformers import pipeline
+
+def psych(request):
+    embedding2 = GooglePalmEmbeddings(google_api_key="AIzaSyBysL_SjXQkJ8lI1WPTz4VwyH6fxHijGUE")
+    vdb_chunks_HF = FAISS.load_local("query/vdb_chunks_HF", embedding2, index_name="indexPsych")
+    query = request.GET.get('query', '')
+
+    # Check if the user's query contains the word "summary"
+    generate_summary = "summary" in query.lower()
+
+    # Retrieve relevant documents
+    ans = vdb_chunks_HF.as_retriever().get_relevant_documents(query)
+
+    # Prepare answers based on whether to generate summary or not
+    if generate_summary:
+        answers = []
+        summary = ""
+
+        if ans:
+            # Generate summaries using the Transformers library
+            summarization_pipeline = pipeline("summarization")
+            answers = [doc.page_content for doc in ans]
+            concatenated_text = " ".join(answers)
+            summary = summarization_pipeline(concatenated_text, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
+    else:
+        answers = [doc.page_content for doc in ans] if ans else []
+        summary = None
+
+    # Return answers and summary as a JSON response
+    return JsonResponse({'answers': answers, 'summary': summary})
